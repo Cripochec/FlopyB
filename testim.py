b473cba1844d828b569fa0dbd83cabd4a1646813
Cripochec <124876135+Cripochec@users.noreply.github.com>
2023-12-04 11:57:14 +0500

    abgrade testim.py

--- testim.py
+++ testim.py
@@ -3,20 +3,10 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/api', methods=['POST'])
-def process_api_request():
-    try:
-        data = request.get_json()
-        received_message = data['message']
-
-        # Здесь вы можете обработать полученное сообщение по вашему усмотрению
-        # В данном примере мы просто отправляем обратно то же самое сообщение
-
-        response_data = {'message': received_message}
-        return jsonify(response_data)
-
-    except Exception as e:
-        print(str(e))
-        return jsonify({'error': 'Something went wrong'}), 500
+def process_data():
+    data_from_client = request.data.decode('utf-8')
+    processed_data = data_from_client + " hello People"
+    return jsonify(result=processed_data)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
